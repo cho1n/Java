0abdb621ef2be4f3a36033be0f2e039f8b9aaae0
cho1n <cjaewon0217@naver.com>
2023-03-07 14:18:34 +0900

    int

--- App.java
+++ App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       // System.out.println("hello wor");
+       int data = 10, hello;
+
+       data = 20;
+
+       hello = data;
+
+       System.out.println(data + " " + hello);
     }
 }
