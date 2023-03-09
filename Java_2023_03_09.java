b87856c7465152a8376784db75000d0b13b63d66
cho1n <cjaewon0217@naver.com>
2023-03-09 13:29:30 +0900

    03_09

---
+++ Java_2023_03_09.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Java_2023_03_09 {
+    
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("> ");
+
+        int data = sc.nextInt();
+        int data2 = sc.nextInt();
+        int data3 = sc.nextInt();
+        System.out.print("> " + data3 + " " + data2 + " " + data);
+
+
+    }
+}
