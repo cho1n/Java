92c2a6cb95cb415aa0b42150ae627241df657302
cho1n <cjaewon0217@naver.com>
2023-03-14 14:27:09 +0900

    last

--- Java_2023_03_14.java
+++ Java_2023_03_14.java
@@ -2,11 +2,18 @@ import java.util.Scanner;
 
 public class Java_2023_03_14 {
     public static void main(String[] args) throws Exception{
-        Scanner sc = new Scanner(System.in);
+        /* Scanner sc = new Scanner(System.in);
         System.out.print("이름 입력 : ");
         String str = sc.next();
 
         System.out.println("> 이름은 " + str + "입니다.");
-        sc.close();
+        sc.close(); */
+
+        int data, ans;
+
+        data = 10 ;
+        System.out.println(data);
+        ans = data++;
+        System.out.println(ans);
     }
 }
